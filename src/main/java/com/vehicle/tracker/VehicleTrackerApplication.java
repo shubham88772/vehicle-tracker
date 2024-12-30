507ad59f1c3ae31fee17854c02a07c6f1a18aa52
shubham88772 <shubham88772@gmail.com>
2024-12-30 06:51:30 +0530

    Project Initiated

---
+++ src/main/java/com/vehicle/tracker/VehicleTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.vehicle.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleTrackerApplication.class, args);
+	}
+
+}
